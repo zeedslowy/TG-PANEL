eb5f9c2a34b554a1a22116a9cd76458c53beac49
ᴛʜᴇ ᴢᴇᴇᴅ <redcereny@hotmail.com>
2024-07-08 23:43:35 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Telegram Bot Yönetim Paneli</title>
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+        }
+        h1 {
+            text-align: center;
+            margin-top: 50px;
+        }
+        .bot-list {
+            list-style-type: none;
+            padding: 0;
+        }
+        .bot-list li {
+            background-color: #333;
+            padding: 10px;
+            margin-bottom: 10px;
+            border-radius: 5px;
+        }
+        .bot-list li a {
+            color: white;
+            text-decoration: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Telegram Bot Yönetim Paneli</h1>
+        <ul class="bot-list">
+            <li><a href="bot1.php">Bot 1</a></li>
+            <li><a href="bot2.php">Bot 2</a></li>
+            <li><a href="bot3.php">Bot 3</a></li>
+        </ul>
+    </div>
+</body>
+</html>
