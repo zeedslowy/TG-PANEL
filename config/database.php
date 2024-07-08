17a6401d7afe387c370ff0c0e7d95fd19082f144
ᴛʜᴇ ᴢᴇᴇᴅ <redcereny@hotmail.com>
2024-07-08 23:56:44 +0300

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,14 @@
+<?php
+// Database connection details
+$servername = "localhost";
+$username = "your_username";
+$password = "your_password";
+$database = "your_database";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
